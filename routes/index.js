54e3122db43196a371bc1c15a4c41f6cb4918160
maoyoug <maoyoug@jumei.com>
2019-05-07 15:30:27 +0800

    重装sequelize mysql mysql2依赖

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
-const ArticleController = require('../controller/article');
+module.exports = (router) => {
+    router.get('/index', async function(ctx, next) {
+        ctx.state = {
+            title: 'koa2 title'
+        };
 
-module.exports =  (router) => {
-  router.get('/index', async function (ctx, next) {
-    ctx.state = {
-      title: 'koa2 title'
-    };
-
-  router.post('/article', ArticleController.create);
-
-  router.get('/article/:id', ArticleController.detail);
-
-    await ctx.render('index', {title: ctx.state});
-  })
+        await ctx.render('index', { title: ctx.state });
+    })
 }
